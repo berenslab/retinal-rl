9bb2ef9ca3f098f7a4eddf4d7a7ec89101742e3c
alex404 <alex404@mailbox.org>
2023-04-01 07:05:45 +0200

    ... and registered observer

--- train.py
+++ train.py
@@ -1,12 +1,28 @@
 import sys
 from sample_factory.cfg.arguments import parse_full_cfg, parse_sf_args
-from sample_factory.train import run_rl
+from sample_factory.train import make_runner
+from sample_factory.utils.typing import Config
+from sample_factory.algo.utils.misc import ExperimentStatus
 
 from retinal_rl.system.encoders import register_retinal_model
-from retinal_rl.system.environment import register_retinal_envs
+from retinal_rl.system.environment import register_retinal_envs,RetinalAlgoObserver
 from retinal_rl.system.arguments import retinal_override_defaults,add_retinal_env_args,add_retinal_env_eval_args
 
 
+### Runner ###
+
+def run_rl(cfg: Config):
+    cfg, runner = make_runner(cfg)
+    runner.register_observer(RetinalAlgoObserver(cfg))
+
+    # here we can register additional message or summary handlers
+    # see sf_examples/dmlab/train_dmlab.py for example
+
+    status = runner.init()
+    if status == ExperimentStatus.SUCCESS:
+        status = runner.run()
+
+    return status
 ### Main ###
 
 
