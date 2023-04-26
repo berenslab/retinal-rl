2d660adc3ccde3de2e3e93f34edcacdd5b42b33e
alex404 <alex404@mailbox.org>
2023-04-26 11:04:57 +0200

    Added a pooling based lindsey network that seems to perform really well without stride

--- retinal_rl/system/encoders.py
+++ retinal_rl/system/encoders.py
@@ -2,6 +2,7 @@
 retina_rl library
 
 """
+#import torch
 from torch import nn
 
 from sample_factory.model.encoder import Encoder
@@ -15,7 +16,8 @@ from sample_factory.algo.utils.context import global_model_factory
 
 
 def register_retinal_model():
-    global_model_factory().register_encoder_factory(make_lindsey_encoder)
+    #global_model_factory().register_encoder_factory(make_lindsey_encoder)
+    global_model_factory().register_encoder_factory(make_maxpool_lindsey_encoder)
 
 
 ### Retinal-VVS Model ###
@@ -24,6 +26,11 @@ def register_retinal_model():
 def make_lindsey_encoder(cfg: Config, obs_space: ObsSpace) -> Encoder:
     """Factory function as required by the API."""
     return LindseyEncoder(cfg, obs_space)
+
+def make_maxpool_lindsey_encoder(cfg: Config, obs_space: ObsSpace) -> Encoder:
+    """Factory function as required by the API."""
+    return LindseyEncoderMaxPool(cfg, obs_space)
+
 
 
 def activation(cfg: Config) -> nn.Module:
@@ -43,8 +50,12 @@ class LindseyEncoder(Encoder):
     def __init__(self, cfg: Config, obs_space: ObsSpace):
         super().__init__(cfg)
 
-        # reuse the default image encoder
+        #self.basic_encoder = torch.jit.script(LindseyEncoderBase(cfg, obs_space["obs"]))
         self.basic_encoder = LindseyEncoderBase(cfg, obs_space["obs"])
+
+        self.encoder_out_size = self.basic_encoder.get_out_size()
+
+        log.debug("Policy head output size: %r", self.get_out_size())
         self.encoder_out_size = self.basic_encoder.get_out_size()
 
         log.debug("Policy head output size: %r", self.get_out_size())
@@ -102,3 +113,73 @@ class LindseyEncoderBase(Encoder):
 
     def get_out_size(self) -> int:
         return self.encoder_out_size
+
+# Copy LindseyEncoderBase to make a version with max pooling at every layer
+class LindseyEncoderBaseMaxPool(Encoder):
+
+    def __init__(self, cfg : Config , obs_space : ObsSpace):
+
+        super().__init__(cfg)
+
+        nchns = cfg.global_channels
+        btlchns = cfg.retinal_bottleneck
+        vvsdpth = cfg.vvs_depth
+        krnsz = cfg.kernel_size
+
+        self.nl_fc = activation(cfg)
+
+        self.kernel_size = krnsz
+
+        # Preparing Conv Layers
+        conv_layers = []
+        self.nls = []
+        for i in range(vvsdpth+2): # +2 for the first 'retinal' layers
+
+            self.nls.append(activation(cfg))
+
+            if i == 0: # 'bipolar cells' ('global channels')
+                conv_layers.extend([nn.Conv2d(3, nchns, krnsz, stride=1), nn.MaxPool2d(2), self.nls[i]])
+            elif i == 1: # 'ganglion cells' ('retinal bottleneck')
+                conv_layers.extend([nn.Conv2d(nchns, btlchns, krnsz, stride=1), self.nls[i]])
+            elif i == 2: # 'V1' ('global channels')
+                conv_layers.extend([nn.Conv2d(btlchns, nchns, krnsz, stride=1), nn.MaxPool2d(2), self.nls[i]])
+            else: # 'vvs layers'
+                conv_layers.extend([nn.Conv2d(nchns, nchns, krnsz, stride=1), self.nls[i]])
+
+        self.conv_head = nn.Sequential(*conv_layers)
+        self.conv_head_out_size = calc_num_elements(self.conv_head, obs_space.shape)
+        self.encoder_out_size = cfg.rnn_size
+        self.fc1 = nn.Linear(self.conv_head_out_size,self.encoder_out_size)
+
+    def forward(self, x):
+
+        x = self.conv_head(x)
+        x = x.contiguous().view(-1, self.conv_head_out_size)
+        x = self.nl_fc(self.fc1(x))
+        return x
+
+    def get_out_size(self) -> int:
+        return self.encoder_out_size
+
+
+class LindseyEncoderMaxPool(Encoder):
+
+    def __init__(self, cfg: Config, obs_space: ObsSpace):
+
+        super().__init__(cfg)
+
+        #self.basic_encoder = torch.jit.script(LindseyEncoderBaseMaxPool(cfg, obs_space["obs"]))
+        self.basic_encoder = LindseyEncoderBaseMaxPool(cfg, obs_space["obs"])
+
+        self.encoder_out_size = self.basic_encoder.get_out_size()
+
+        log.debug("Policy head output size: %r", self.get_out_size())
+
+    def forward(self, obs_dict):
+        x = self.basic_encoder(obs_dict["obs"])
+        return x
+
+    def get_out_size(self) -> int:
+        return self.encoder_out_size
+
+
