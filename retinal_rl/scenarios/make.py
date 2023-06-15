a2d5f48c1b908d54dde8e9f58b78b9a2020f2e12
alex404 <alex404@mailbox.org>
2023-06-15 17:49:51 +0200

    Untested alpha version in place

--- retinal_rl/scenarios/make.py
+++ retinal_rl/scenarios/make.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 import subprocess
 import os.path as osp
 
@@ -6,16 +7,141 @@ import yaml
 import omg
 
 
-### Scenario config ###
+### Load Config ###
 
 
+def load_config(flnm):
+    with open("scenarios/{0}.yaml".format(flnm)) as stream:
+        return yaml.safe_load(stream)
+
+object_types = ['nourishment','poison','obstacle','distractor']
+
+### Building ACS files ###
+
+def make_acs(cfg,actor_names,actor_num_textures):
+    
+    acs = "actor"
+
+    # Directives
+    acs +="""// Directives
+#import "gathering.acs"
+#include "zcommon.acs"
+
+script "Load Config Information" OPEN {{
+
+    // Metabolic variables
+    metabolic_delay = {0};
+    metabolic_damage = {1};
+    """.format(cfg['metabolic']['delay'],cfg['metabolic']['damage'])
+
+    for typ in object_types:
+        xcfg = cfg[typ]
+        acs += """
+    // {0} variables
+    {0}_unique = {1};
+    {0}_init = {2};
+    {0}_delay = {3};
+    """.format(typ,len(xcfg['actors']),xcfg['init'],xcfg['delay'])
+
+    acs += "\n// Loading arrays"
+
+    for i,(actor_name,num_textures) in enumerate(zip(actor_names,actor_num_textures)):
+        acs += """
+    actor_names[{0}] = "{1}";
+    actor_num_textures[{0}] = {2};
+    """.format(i,actor_name,num_textures)
+
+    acs += "\n}"
+
+    return acs
+
+### Building Decorate Files ###
+
+decorate_pre = {}
+
+decorate_pre['nourishment'] = """ACTOR {0} : CustomInventory
+{{
+    +INVENTORY.ALWAYSPICKUP
+    States
+    {{
+    Pickup:
+        TNT1 A 0 HealThing({1});
+        Stop;
+        """
+decorate_pre['poison'] = """ACTOR {0} : CustomInventory
+{{
+    +INVENTORY.ALWAYSPICKUP
+    States
+    {{
+    Pickup:
+        TNT1 A 0 DamageThing({1});
+        Stop;
+        """
+decorate_pre['obstacle'] = """ACTOR {0}
+{{
+    Radius 30
+    Height 300
+    +SOLID
+    States
+    {{
+        """
+decorate_pre['distractor'] = """ACTOR {0} : CustomInventory
+{{
+    +INVENTORY.ALWAYSPICKUP
+    States
+    {{
+        """
+
+def actor_code(i,j):
+    # Convert k to caps alpha string
+    return chr(65 + i) + texture_code(j)
+
+def texture_code(j):
+    # Convert k to a 3-digit alpha all-caps string
+    return chr(65 + j // 26 ** 2) + chr(65 + (j // 26) % 26) + chr(65 + j % 26)
+
+def decorate_actor(cfg,actor_name,typ,actor_idx,num_textures):
+
+    cfg = cfg['typ']['actor_name']
+
+    decorate = ""
+    if typ == "nourishment":
+        decorate += decorate_pre[typ].format(actor_name,cfg['healing'])
+    elif type == "poison":
+        decorate += decorate_pre[typ].format(actor_name,cfg['damage'])
+    elif type == "obstacle":
+        decorate += decorate_pre[typ].format(actor_name)
+    elif type == "distractor":
+        decorate += decorate_pre[typ].format(actor_name)
+    else:
+        raise ValueError("Invalid actor type: {0}".format(typ))
+
+    # Multiline string for beginning of decorate file
+    for j in range(num_textures):
+        decorate += """
+            Tex{0}:
+                {0} A -1""".format(actor_code(actor_idx,j))
+
+    decorate += """
+            }
+    }\n\n"""
+
+    return decorate
+
+### Loading Textures ###
+
+def load_textures(wad,actor_idx,pngs):
+
+    for j,png in enumerate(pngs):
+        code = actor_code(actor_idx,j) + "A0"
+        wad.sprites[code] = omg.Graphic(from_file=png)
 
 ### Creating Scenarios ###
 
 
 def create_base_wad():
 
-    bpth = "scenarios/resources/base"
+    bpth = "resources/base"
     wad = omg.WAD()
 
     grspth = osp.join(bpth,"grass.png")
@@ -37,97 +163,54 @@ def create_base_wad():
 
     return wad,mpgrp
 
+def make_scenario(scnnm):
 
-### Load Config ###
-
-
-def load_config(flnm):
-    with open("scenarios/{0}.yaml".format(flnm)) as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-
-
-### Building Decorate Files ###
-
-
-def actor_code(i,j):
-    # Convert k to caps alpha string
-    return chr(65 + i) + texture_code(j)
-
-def texture_code(j):
-    # Convert k to a 3-digit alpha all-caps string
-    return chr(65 + j // 26 ** 2) + chr(65 + (j // 26) % 26) + chr(65 + j % 26)
-
-def decorate_actor(actor_name,actor_idx,num_textures):
-
-    # Multiline string for beginning of decorate file
-    decorate = """ACTOR {0} : Inventory
-    {{
-        +INVENTORY.ALWAYSPICKUP
-        States
-            {{""".format(actor_name)
-
-    for j in range(num_textures):
-        decorate += """
-            Tex{0}:
-                {0} A -1""".format(actor_code(actor_idx,j))
-
-    decorate += """
-            }
-    }\n\n"""
-
-    return decorate
-
-### Textures ###
-
-def load_textures(wad,actor_idx,pngs):
-
-    num_textures = len(pngs)
-
-    for j,png in enumerate(pngs):
-        code = actor_code(actor_idx,j) + "A0"
-        wad.sprites[code] = omg.Graphic(from_file=png)
-
-    return num_textures
-
-def make_scenario(scnnm="gathering_apples"):
-
-    txtdr = osp.join("scenarios/resources/textures",texture)
-    # Get actor_names from base directory names in texture directory
-    actor_names = [d for d in os.listdir(txtdr) if osp.isdir(osp.join(txtdr,d))]
-
-    pngss = []
-    # Get all pngs in each actor directory
-    for actor_name in actor_names:
-        actdr = osp.join(txtdr,actor_name)
-        pngs = [osp.join(actdr,f) for f in os.listdir(actdr) if osp.isfile(osp.join(actdr,f)) and f.endswith(".png")]
-        pngss.append(pngs)
-
-    # Library path names
-    lbonm = task[:8].upper()
-    lbinm = lbonm[:5] + "SRC"
+    # Preloading
+    cfg = load_config(scnnm)
 
     wad,mpgrp = create_base_wad()
 
-    decorate = ""
-
-    for actor_idx,actor_name in enumerate(actor_names):
-        num_textures = load_textures(wad,actor_idx,pngss[actor_idx])
-        decorate += decorate_actor(actor_idx,actor_name,num_textures)
-
-    # Decorate
-    wad.data['DECORATE'] = omg.Lump(data=decorate.encode('utf-8'))
-
-    rscpth = "scenarios/resources"
-    tskpth = osp.join(rscpth,"tasks")
+    # Defining paths
+    rscpth = "resources"
+    bspth = osp.join(rscpth,"base")
+    acspth = osp.join(bspth,"acs")
 
     bhipth = osp.join(rscpth,scnnm + ".acs")
     bhopth = osp.join(rscpth,scnnm + ".o")
 
-    lbipth = osp.join(tskpth,task + ".acs")
-    lbopth = osp.join(tskpth,task + ".o")
+    lbipth = osp.join(acspth,"retinal.acs")
+    lbopth = osp.join(acspth,"retinal.o")
+
+    # Building decorate and loading textures
+
+    decorate = ""
+    actor_names = []
+    actor_num_textures = []
+
+    actor_idx = 0
+    for typ in object_types:
+        for actor_name in cfg[typ]['actors']:
+
+            pngpths = cfg[typ][actor_name]['textures']
+            # get all pngs listend in pngpths and subdirs
+            pngs = []
+            for pngpth in pngpths:
+                pngs += glob.glob(pngpth)
+
+            num_textures = len(pngs)
+            load_textures(wad,actor_idx,pngs)
+            decorate += decorate_actor(cfg,actor_name,typ,actor_idx,num_textures)
+
+            actor_idx += 1
+            actor_names.append(actor_name)
+            actor_num_textures.append(num_textures)
+
+    # Decorate
+    wad.data['DECORATE'] = omg.Lump(data=decorate.encode('utf-8'))
+
+    # Write ACS
+    with open(bhipth,'w') as f:
+        f.write(make_acs(cfg,actor_names,actor_num_textures))
 
     # Compile ACS
     subprocess.call(["acc", "-i","/usr/share/acc", bhipth, bhopth])
@@ -147,56 +230,4 @@ def make_scenario(scnnm="gathering_apples"):
     os.remove(lbopth)
 
     # Save to file
-    wad.to_file(osp.join("scenarios",task + "_" + texture + ".wad"))
-
-def make_gathering_apples():
-
-    task="gathering"
-    texture="apples"
-    # take first 8 letters of task and capitalize
-    lbonm = task[:8].upper()
-    lbinm = lbonm[:5] + "SRC"
-    scnnm = task + "_" + texture
-
-    wad,mpgrp = create_base_wad()
-
-    rscpth = "scenarios/resources"
-    tskpth = osp.join(rscpth,"tasks")
-    txtpth = osp.join(rscpth,"textures",texture)
-
-    rappth = osp.join(txtpth,"red_apple.png")
-    bappth = osp.join(txtpth,"blue_apple.png")
-    decpth = osp.join(txtpth,"apples.decorate")
-
-    bhipth = osp.join(rscpth,scnnm + ".acs")
-    bhopth = osp.join(rscpth,scnnm + ".o")
-
-    lbipth = osp.join(tskpth,task + ".acs")
-    lbopth = osp.join(tskpth,task + ".o")
-
-    # Compile ACS
-    subprocess.call(["acc", "-i","/usr/share/acc", bhipth, bhopth])
-    subprocess.call(["acc", "-i","/usr/share/acc", lbipth, lbopth])
-
-    # Sprites
-    wad.sprites['RAPPA0'] = omg.Graphic(from_file=rappth)
-    wad.sprites['BAPPA0'] = omg.Graphic(from_file=bappth)
-
-    # Decorate
-    wad.data['DECORATE'] = omg.Lump(from_file=decpth)
-
-    # Libraries
-    wad.libraries[lbinm] = omg.Lump(from_file=lbipth)
-    wad.libraries[lbonm] = omg.Lump(from_file=lbopth)
-
-    # Map
-    mpgrp['SCRIPTS'] = omg.Lump(from_file=bhipth)
-    mpgrp['BEHAVIOR'] = omg.Lump(from_file=bhopth)
-    wad.udmfmaps["MAP01"] = omg.UMapEditor(mpgrp).to_lumps()
-
-    # Cleanup
-    os.remove(bhopth)
-    os.remove(lbopth)
-
-    # Save to file
-    wad.to_file(osp.join("scenarios",task + "_" + texture + ".wad"))
+    wad.to_file(osp.join("scenarios",scnnm + ".wad"))
