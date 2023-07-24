9f3656e9102c136b021b0610bd80f63e2e3f3a53
alex404 <alex404@mailbox.org>
2023-07-24 22:28:40 +0200

    Infrastructure

--- make_scenarios.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python make_scenario.py gathering apples --preload
-python make_scenario.py gathering gabors
-python make_scenario.py gathering mnist
-python make_scenario.py gathering cifar10
-
