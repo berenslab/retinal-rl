c4cb02393fa1a226d7f14cfe52f8fe7f78df1c5d
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-07-19 16:33:20 +0200

    feat: add default cfg for sf as dataclass

--- retinal_rl/rl/sample_factory/sf_engine.py
+++ retinal_rl/rl/sample_factory/sf_engine.py
@@ -1,10 +1,13 @@
 
+from typing import Tuple
 from omegaconf import DictConfig
 import torch
 
 from retinal_rl.models.brain import Brain
 from retinal_rl.rl.interface import RLEngine
 from sample_factory.model.actor_critic import ActorCritic
+from sample_factory.utils.typing import ActionSpace, Config, ObsSpace
+from retinal_rl.rl.sample_factory.config_defaults import SfDefaults
 
 class SFEngine(RLEngine):
     def train(
@@ -13,6 +16,13 @@ class SFEngine(RLEngine):
         brain: Brain
     ):
         pass
+    
+    def unpack_cfg(cfg: DictConfig, brain:Brain) -> Tuple[ObsSpace, ActionSpace, Config]:
+        obs_space = ObsSpace() # Get obs space from cfg or brain model
+        action_space = ActionSpace() # Get action space from cfg or brain model
+        sf_cfg = SfDefaults() # Load Defaults
+        # TODO: merge cfg and sf_cfg
+        return obs_space, action_space, sf_cfg
 
 def actor_critic_from_brain(brain: Brain) -> ActorCritic:
     assert brain.connectome
