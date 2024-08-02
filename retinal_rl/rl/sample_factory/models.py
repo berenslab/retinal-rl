1a32b7017e148e3dc5ead7b8814a0d1cbde8c62d
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-08-02 15:00:00 +0200

    feat: implement a (hopefully?) working first version of a brain wrapper for sample_factory

--- retinal_rl/rl/sample_factory/models.py
+++ retinal_rl/rl/sample_factory/models.py
@@ -8,12 +8,19 @@ from sample_factory.algo.utils.context import global_model_factory
 from sample_factory.algo.utils.tensor_dict import TensorDict
 from torch import Tensor
 import torch
+import networkx as nx
 from retinal_rl.models.brain import Brain
 from retinal_rl.rl.interface import BrainInterface
 from retinal_rl.rl.sample_factory.sf_interfaces import ActorCriticProtocol
+import warnings
+from enum import Enum
+from torch import nn
 
-def default_cfg():
-    ...
+class CoreMode(Enum):
+    IDENTITY = 0,
+    SIMPLE = 1,
+    RNN = 2,
+    MULTI_MODULES = 1,
 
 
 class SampleFactoryBrain(ActorCritic, ActorCriticProtocol, BrainInterface):
@@ -22,9 +29,9 @@ class SampleFactoryBrain(ActorCritic, ActorCriticProtocol, BrainInterface):
 
         self.set_brain(Brain(**cfg.brain)) # TODO: Find way to instantiate brain outside
 
-        self.action_parameterization = self.get_action_parameterization(
-            self.decoder.get_out_size()
-        )
+        decoder_out_size = self.brain.circuits[self.decoder_name].output_shape
+        self.critic_linear = nn.Linear(decoder_out_size, 1)
+        self.action_parameterization = self.get_action_parameterization(decoder_out_size)
 
     def set_brain(self, brain: Brain):
         """
@@ -32,27 +39,91 @@ class SampleFactoryBrain(ActorCritic, ActorCriticProtocol, BrainInterface):
         Checks for brain compatibility.
         Decide which part of the brain is head/core/tail or creates Identity transforms if needed.
         """
-        if self.check_brain_compatible(brain):
-            self.brain = brain
+        enc, core, dec = self.get_encoder_decoder(brain)
+        self.brain = brain
+        self.encoder_name = enc
+        self.decoder_name = dec
+        self.core_mode = core
 
-    def check_brain_compatible(self, brain: Brain):
-        raise NotImplementedError()
+
+    @staticmethod
+    def get_encoder_decoder(brain: Brain):
+        assert "vision" in brain.sensors.keys() # needed as input
+        # potential TODO: add other input sources if needed?
+
+        vision_paths = []
+        for node in brain.connectome:
+            if brain.connectome.out_degree(node)==0: #it's a leaf
+                vision_paths.append(nx.shortest_path(brain.connectome, "vision", node))
+
+        decoder = "action_decoder" # default assumption
+        if decoder in brain.circuits.keys(): # needed to produce output = decoder
+            vision_path = nx.shortest_path(brain.connectome, "vision", "action_decoder")
+        else:
+            vision_path = vision_paths[0]
+            decoder = vision_path[-1]
+            warnings.warn("No action decoder in model. Will use " + decoder + " instead.")
+
+        encoder = vision_path[1]
+        if len(vision_path) == 4:
+            core = CoreMode.SIMPLE
+            CoreMode.SIMPLE.value = vision_path[2] # use center module as core
+            #TODO: Check if recurrent!
+        if len(vision_path) < 4:
+            core = CoreMode.IDENTITY
+            warnings.warn("Seems like there is no model core. Will use an Identity.")
+        else: # more than four
+            core = CoreMode.MULTI_MODULES
+            warnings.warn("Will use multiple modules as core: " + mod_name for mod_name in vision_path[2:-1])
+
+        return encoder, core, decoder
 
     def forward_head(self, normalized_obs_dict: Dict[str, Tensor]) -> Tensor:
-        raise NotImplementedError()
+        vision_input = normalized_obs_dict["obs"]
+        return self.brain.circuits[self.encoder_name](vision_input)
 
     def forward_core(self, head_output, rnn_states):
-        raise NotImplementedError()
+        #TODO: what to do with rnn states?
+        if self.core_mode == CoreMode.SIMPLE:
+            out = self.brain.circuits[self.core_mode.value](head_output)
+        elif self.core_mode == CoreMode.RNN:
+            out, rnn_states = self.brain.circuits[self.core_mode.value](head_output, rnn_states)
+        elif self.core_mode == CoreMode.IDENTITY:
+            out = head_output
+        if self.core_mode == CoreMode.MULTI_MODULES:
+            out = head_output #TODO: Implement partial forward (in brain!)
+        return out, rnn_states
 
     def forward_tail(
         self, core_output, values_only: bool, sample_actions: bool
     ) -> TensorDict:
-        raise NotImplementedError()
+        out = self.brain.circuits[self.decoder_name](core_output)
+
+        values = self.critic_linear(out).squeeze()
+
+        result = TensorDict(values=values)
+        if values_only:
+            return result
+
+        action_distribution_params, self.last_action_distribution = self.action_parameterization(out)
+
+        # `action_logits` is not the best name here, better would be "action distribution parameters"
+        result["action_logits"] = action_distribution_params
+
+        self._maybe_sample_actions(sample_actions, result)
+        return result
+
 
     def forward(
         self, normalized_obs_dict, rnn_states, values_only: bool = False
     ) -> TensorDict:
-        raise NotImplementedError()
+        head_out = self.forward_head(normalized_obs_dict)
+        core_out, new_rnn_states = self.forward_core(head_out, rnn_states)
+        result = self.forward_tail(core_out)
+        result["new_rnn_states"] = new_rnn_states
+        result["latent_states"] = core_out
+        return result
+
 
     def get_brain(self) -> Brain:
         return self.brain
