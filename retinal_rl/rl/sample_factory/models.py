5129c5036bcc1a77f3a4a0b657d027834ac7c812
Fabio Seel <fabio.seel@uni-tuebingen.de>
2024-12-12 16:22:13 +0100

    fix: increase flexibility of SampleFactoryBrain wrt Brain (simple core, more flexible encoder)

--- retinal_rl/rl/sample_factory/models.py
+++ retinal_rl/rl/sample_factory/models.py
@@ -1,6 +1,6 @@
 import warnings
 from enum import Enum
-from typing import Dict, Optional, Tuple
+from typing import Optional
 
 import networkx as nx
 import numpy as np
@@ -23,7 +23,6 @@ class CoreMode(Enum):
     RNN = (2,)
     MULTI_MODULES = (3,)
 
-
 class SampleFactoryBrain(ActorCritic, ActorCriticProtocol):
     def __init__(self, cfg: Config, obs_space: ObsSpace, action_space: ActionSpace):
         # Attention: make_actor_critic passes [cfg, obs_space, action_space], but ActorCritic takes the reversed order of arguments [obs_space, action_space, cfg]
@@ -32,8 +31,8 @@ class SampleFactoryBrain(ActorCritic, ActorCriticProtocol):
         self.set_brain(create_brain(DictConfig(cfg.brain)))
         # TODO: Find way to instantiate brain outside
 
-        dec_out_shape = self.brain.circuits[self.decoder_name].output_shape
-        decoder_out_size = np.prod(dec_out_shape)
+        dec_out_shape = self.brain.circuits[self.decoder_names[-1]].output_shape
+        decoder_out_size = int(np.prod(dec_out_shape))
         self.critic_linear = nn.Linear(decoder_out_size, 1)
         self.action_parameterization = self.get_action_parameterization(
             decoder_out_size
@@ -48,82 +47,86 @@ class SampleFactoryBrain(ActorCritic, ActorCriticProtocol):
         """
         enc, core, dec = self.get_encoder_decoder(brain)
         self.brain = brain
-        self.encoder_name = enc
+        self.encoder_names = enc
         self.core_mode = core
-        self.decoder_name = dec
+        self.decoder_names = dec
 
     @staticmethod
-    def get_encoder_decoder(brain: Brain) -> Tuple[str, CoreMode, str]:
+    def get_encoder_decoder(brain: Brain) -> tuple[list[str], CoreMode, list[str]]:
         assert "vision" in brain.sensors  # needed as input
         # potential TODO: add other input sources if needed?
 
-        vision_paths = []
+        vision_paths: list[list[str]] = []
         for node in brain.connectome:
             if brain.connectome.out_degree(node) == 0:  # it's a leaf
                 vision_paths.append(nx.shortest_path(brain.connectome, "vision", node))
 
         decoder = "action_decoder"  # default assumption
         if decoder in brain.circuits:  # needed to produce output = decoder
-            vision_path = nx.shortest_path(brain.connectome, "vision", "action_decoder")
+            vision_path: list[str] = nx.shortest_path(brain.connectome, "vision", "action_decoder")
         else:
             selected_path = 0
-            out_dim = np.inf
+            out_dim = None
             for i, vision_path in enumerate(
                 vision_paths
             ):  # Assuming that the path with the smallest output dimension is best for action prediction (eg in contrast to a decoder trying to reproduce the input)
                 dec_out_shape = brain.circuits[vision_path[-1]].output_shape
-                if np.prod(dec_out_shape) < out_dim:
-                    out_dim = np.prod(dec_out_shape)
+                if not(out_dim) or np.prod(dec_out_shape) < out_dim:
+                    out_dim = int(np.prod(dec_out_shape))
                     selected_path = i
             vision_path = vision_paths[selected_path]
-            decoder = vision_path[-1]
             warnings.warn(
-                "No action_decoder in model. Will use " + decoder + " instead."
+                message = "No action_decoder in model. Will use " + vision_path[-1] + " instead."
             )
 
-        encoder = vision_path[1]
-        if len(vision_path) == 4:
-            core = CoreMode.SIMPLE
-            CoreMode.SIMPLE.value = vision_path[2]  # use center module as core
-            # TODO: Check if recurrent!
-        if len(vision_path) < 4:
-            core = CoreMode.IDENTITY
+        core = "core" # default assumption
+        if core in vision_path:
+            encoder_end = vision_path.index(core)
+
+            core_mode = CoreMode.SIMPLE
+            CoreMode.SIMPLE.value = core
+            encoder = vision_path[1:encoder_end]
+            decoder = vision_path[encoder_end:-1]
+        else: # TODO: Multi Module and RNN Core
+            core_mode= CoreMode.IDENTITY
+            encoder = vision_path[1:-1]
+            decoder = [vision_path[-1]]
             warnings.warn("Seems like there is no model core. Will use an Identity.")
-        else:  # more than four
-            core = CoreMode.MULTI_MODULES
-            warnings.warn(
-                "Will use multiple modules as core: "
-                + ", ".join([mod_name for mod_name in vision_path[2:-1]])
-            )
 
-        return encoder, core, decoder
+        return encoder, core_mode, decoder
 
-    def forward_head(self, normalized_obs_dict: Dict[str, Tensor]) -> Tensor:
-        vision_input = normalized_obs_dict["obs"]
-        return self.brain.circuits[self.encoder_name](vision_input)
+    def forward_head(self, normalized_obs_dict: dict[str, Tensor]) -> Tensor:
+        out = normalized_obs_dict["obs"]
+        for enc in self.encoder_names:
+            out = self.brain.circuits[enc](out)
+        return out
 
-    def forward_core(self, head_output, rnn_states):
+    def forward_core(self, head_output: Tensor, rnn_states: Tensor):
         # TODO: what to do with rnn states? -> implement neural circuit that is capable?! (could also be done by some module internally, right?)
         if self.core_mode == CoreMode.SIMPLE:
-            out = self.brain.circuits[self.core_mode.value](head_output)
+            out = self.brain.circuits[str(self.core_mode.value)](head_output)
         elif self.core_mode == CoreMode.RNN:
-            out, rnn_states = self.brain.circuits[self.core_mode.value](
+            out, rnn_states = self.brain.circuits[str(self.core_mode.value)](
                 head_output, rnn_states
             )
         elif self.core_mode == CoreMode.IDENTITY:
             out = head_output
-        if self.core_mode == CoreMode.MULTI_MODULES:
-            out = head_output  # TODO: Implement partial forward (in brain!)
+        else: #self.core_mode == CoreMode.MULTI_MODULES:
+            out = head_output
+            for m in self.core_mode.value:
+                out = self.brain.circuits[str(m)](out)
         return out, rnn_states
 
     def forward_tail(
         self,
-        core_output,
+        core_output: Tensor,
         values_only: bool,
         sample_actions: bool,
         action_mask: Optional[Tensor] = None,
     ) -> TensorDict:
-        out = self.brain.circuits[self.decoder_name](core_output)
+        out = core_output
+        for m in self.decoder_names:
+            out = self.brain.circuits[m](core_output)
         out = torch.flatten(out, 1)
 
         values = self.critic_linear(out).squeeze()
@@ -144,8 +147,8 @@ class SampleFactoryBrain(ActorCritic, ActorCriticProtocol):
 
     def forward(
         self,
-        normalized_obs_dict,
-        rnn_states,
+        normalized_obs_dict: dict[str, Tensor],
+        rnn_states: Tensor,
         values_only: bool = False,
         action_mask: Optional[Tensor] = None,
     ) -> TensorDict:
