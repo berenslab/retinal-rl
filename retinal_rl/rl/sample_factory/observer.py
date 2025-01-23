9a4ff0778aa37c320f35552e2ef0afd62aa39f9b
Fabio Seel <fabio.seel@uni-tuebingen.de>
2025-01-23 13:51:10 +0100

    fix: update observer for use with brain model + analyze function

--- retinal_rl/rl/sample_factory/observer.py
+++ retinal_rl/rl/sample_factory/observer.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import os
+from pathlib import Path
 
 import torch
 import wandb
@@ -8,16 +9,18 @@ from sample_factory.algo.runners.runner import AlgoObserver, Runner
 from sample_factory.utils.typing import Config
 from sample_factory.utils.utils import debug_log_every_n, log
 
+from retinal_rl.models.brain import Brain
 from retinal_rl.models.objective import Objective
 from retinal_rl.rl.analyze import AnalysesCfg, analyze
 from retinal_rl.rl.loss import RLContext
+from retinal_rl.rl.sample_factory.models import SampleFactoryBrain
+from retinal_rl.rl.sample_factory.util import load_brain_from_checkpoint
 from retinal_rl.rl.util import (
     analysis_root,
     plot_path,
     read_analysis_count,
     write_analysis_count,
 )
-from runner.frameworks.rl.sf_framework import SFFramework
 
 multiprocessing.set_start_method(
     "spawn", force=True
@@ -38,77 +41,53 @@ class RetinalAlgoObserver(AlgoObserver):
         self.queue = multiprocessing.Queue()
 
         # get analysis count
-        if not os.path.exists(analysis_root(cfg)):
-            os.makedirs(analysis_root(cfg))
+        # if not os.path.exists(analysis_root(cfg)):
+        #     os.makedirs(analysis_root(cfg))
 
-        acount = read_analysis_count(cfg)
+        # acount = read_analysis_count(cfg)
 
-        self.steps_complete = acount
+        self.steps_complete = 0  # Skip step 0 as no model is saved yet
 
-    def analyze(self, queue : multiprocessing.Queue[None], env_step: int):
+    def _analyze(self, env_step: int):
+        try:
+            brain = load_brain_from_checkpoint(self.cfg)
+            brain.sensors['vision'] = (3,160,160) # TODO: Hardcore Hack cause there's a bug in rf estimation
+            objective: Objective[RLContext] = instantiate(self.cfg.objective, brain=brain)
+            cfg = AnalysesCfg(
+                Path(self.cfg.run_dir),
+                Path(self.cfg.plot_dir),
+                Path(self.cfg.checkpoint_plot_dir),
+                Path(self.cfg.data_dir),
+                self.cfg.with_wandb,
+            )
+            epoch = env_step  # use env_step instead of epoch for logging
+            analyze(
+                cfg, torch.device("cuda"), brain, objective, epoch, copy_checkpoint=False
+            )
+        except Exception as e:
+            log.error(f"Analysis failed: {e}")
+
+    def _detach_analyze(self, queue, env_step: int):
         """Run analysis in a separate process."""
+        self._analyze(env_step)
 
-        brain = SFFramework.load_brain_from_checkpoint(self.cfg)
-        objective: Objective[RLContext] = instantiate(self.cfg.objective, brain=brain)
-        cfg = AnalysesCfg(cfg.run_dir, cfg.plot_dir, cfg.checkpoint_plot_dir, cfg.data_dir, cfg.with_wandb)
-        epoch = env_step # use env_step instead of epoch for logging
-        analyze(cfg, torch.device('cuda'), brain, objective, epoch, copy_checkpoint=False)
         # envstps = analyze(self.cfg, progress_bar=False)
         # queue.put(envstps, block=False)
+        queue.put(0, block=False)
 
     def on_training_step(
         self, runner: Runner, training_iteration_since_resume: int
     ) -> None:  # TODO: deprecated and will be refactored anyway
         """Called after each training step."""
         # TODO: Check and refactor
-        if self.current_process is None:
-            total_env_steps = sum(runner.env_steps.values())
-            current_step = total_env_steps // self.freq
+        total_env_steps = sum(runner.env_steps.values())
+        current_step = total_env_steps // self.freq
 
-            msg = (
-                "RETINAL RL: No analysis running. current_step = %d, steps_complete = %d"
-                % (current_step, self.steps_complete)
+        if current_step >= self.steps_complete:
+            # run analysis in a separate process
+            log.debug(
+                "RETINAL RL: current_step >= self.steps_complete, launching analysis process..."
             )
-            debug_log_every_n(100, msg)
 
-            if current_step >= self.steps_complete:
-                # run analysis in a separate process
-                log.debug(
-                    "RETINAL RL: current_step >= self.steps_complete, launching analysis process..."
-                )
-                self.current_process = multiprocessing.Process(
-                    target=self.analyze, args=(self.queue,)
-                )
-                self.current_process.start()
-
-        else:
-            if not self.current_process.is_alive():
-                if self.current_process.exitcode == 0:
-                    log.debug(
-                        "RETINAL RL: Analysis process finished successfully. Retrieving envstps..."
-                    )
-                    envstps = self.queue.get()
-                    ana_name = "env_steps-" + str(envstps)
-
-                    if self.cfg.with_wandb:
-                        log.debug("RETINAL RL: Uploading plots to wandb...")
-
-                        pltpth = plot_path(self.cfg, ana_name)
-                        # Recursively list all files in pltpth
-                        for path, _, files in os.walk(pltpth):
-                            # upload all pngs to wandb
-                            for f in files:
-                                if f.endswith(".png"):
-                                    log.debug("RETINAL RL: Uploading %s", f)
-                                    wandb.log({f: wandb.Image(os.path.join(path, f))})
-                            # Upload video to wandb
-                            for f in files:
-                                if f.endswith(".mp4"):
-                                    log.debug("RETINAL RL: Uploading %s", f)
-                                    wandb.log({f: wandb.Video(os.path.join(path, f))})
-
-                    self.steps_complete += 1
-                    write_analysis_count(self.cfg, self.steps_complete)
-
-                self.current_process.join()
-                self.current_process = None
+            self._analyze(total_env_steps)
+            self.steps_complete += 1
