3a778316e995d2ab67cdc260b01050c24a341eab
Fabio Seel <fabio.seel@uni-tuebingen.de>
2025-06-13 12:04:07 +0200

    fix: disable wandb for tests

--- tests/modules/conftest.py
+++ tests/modules/conftest.py
@@ -47,6 +47,7 @@ def cleanup(config: DictConfig):
 @pytest.fixture
 def classification_config() -> Generator[DictConfig, None, None]:
     _config = config("classification")
+    _config.logging.use_wandb = False  # disable wandb for tests
     yield _config
     cleanup(_config)
 
@@ -54,6 +55,7 @@ def classification_config() -> Generator[DictConfig, None, None]:
 @pytest.fixture
 def rl_config() -> Generator[DictConfig, None, None]:
     _config = config("decoding-rl")
+    _config.logging.use_wandb = False  # disable wandb for tests
     yield _config
     cleanup(_config)
 
