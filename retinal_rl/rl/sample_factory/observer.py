e0950fe086025644667a8bbabc86d251346f4d1f
Fabio Seel <60138972+fabioseel@users.noreply.github.com>
2026-06-11 10:27:05 +0200

    'gabor10' scenarios, log pickup statistics, Pickup reward shaping (#121)
    * feat: implement pickup analysis during training
    
    * fix: misinterpretation of pickup dict values in observer
    
    * fix: pickup frequencies count corrected
    
    * fix: linting error (line length)
    
    * feat: implement pickup reward
    
    * feat: add possibility to have purely the pickup reward
    
    * feat: remove 'living penalty' to encourage longer lives
    
    * fix: reset last health for pickpu tracking, proper logging of unexpected health diff/ pickup value
    
    * chore: comment / explain USER17, enrich comment regarding object values

--- retinal_rl/rl/sample_factory/observer.py
+++ retinal_rl/rl/sample_factory/observer.py
@@ -3,13 +3,14 @@ from pathlib import Path
 
 import torch
 from hydra.utils import instantiate
-from sample_factory.algo.runners.runner import AlgoObserver, Runner
-from sample_factory.utils.typing import Config
+from sample_factory.algo.runners.runner import AlgoObserver, Runner, SummaryWriter
+from sample_factory.utils.typing import Config, PolicyID
 from sample_factory.utils.utils import log
 
 from retinal_rl.models.objective import Objective
 from retinal_rl.rl.analyze import AnalysesCfg, analyze
 from retinal_rl.rl.loss import RLContext
+from retinal_rl.rl.sample_factory.retinal_stats_handler import retinal_stats_handler
 from retinal_rl.rl.sample_factory.util import load_brain_from_checkpoint
 
 multiprocessing.set_start_method(
@@ -80,7 +81,6 @@ class RetinalAlgoObserver(AlgoObserver):
         self, runner: Runner, training_iteration_since_resume: int
     ) -> None:  # TODO: deprecated and will be refactored anyway
         """Called after each training step."""
-        # TODO: Check and refactor
         total_env_steps = sum(runner.env_steps.values())
 
         if total_env_steps >= self.next_analysis_step:
@@ -94,3 +94,28 @@ class RetinalAlgoObserver(AlgoObserver):
                 self.cur_freq, self.end_freq
             )
             self.cur_freq = self.cur_freq * 2
+
+    # Use extra summaries for now TODO: unify with classification logging
+    def extra_summaries(
+        self,
+        runner: Runner,
+        policy_id: PolicyID,
+        summary_writer: SummaryWriter,
+        env_steps: int,
+    ) -> None:
+        # report pickup frequencies
+        if retinal_stats_handler.pickups:
+            pickups = retinal_stats_handler.pickups
+
+            # Reset Pickups dict
+            retinal_stats_handler.pickups = dict()
+
+            total_pickups = max(sum(pickups.values()), 1)  # ensure no division by zero
+
+            for object_value, num_pickups in pickups.items():
+                summary_writer.add_scalar(
+                    f"pickups/{object_value}", num_pickups / total_pickups, env_steps
+                )
+                summary_writer.add_scalar(
+                    f"pickups/abs_{object_value}", num_pickups, env_steps
+                )
